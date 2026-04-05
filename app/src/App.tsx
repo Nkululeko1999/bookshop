41e8551b42c195b3505bdf99a840ab5870d46d2d
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2026-04-05 09:28:20 +0200

    feat: add react queary

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Home from "./pages/home/Home";
 import Books from "./pages/books/Books";
 import NotFound from "./pages/not-found/NotFound";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/books" element={<Books />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
