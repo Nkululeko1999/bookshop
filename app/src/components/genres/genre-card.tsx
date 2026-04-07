c1dcc95f0ecd8098aecb83bc1b3efc2f29785c20
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2026-04-07 08:05:10 +0200

    feat: delete on genre card

--- app/src/components/genres/genre-card.tsx
+++ app/src/components/genres/genre-card.tsx
@@ -1,13 +1,32 @@
+import { Trash } from "lucide-react";
 import { Card, CardContent } from "../ui/card";
 
-const GenreCard = ({ name }: { name: string }) => {
-    return (
-        <Card className="p-4 shadow rounded cursor-pointer border hover:bg-gray-100 transition-all">
-            <CardContent>
-                <h3 className="text-base text-center font-medium">{name}</h3>
-            </CardContent>
-        </Card>
-    );
-}
+const GenreCard = ({
+  id,
+  name,
+  showDelete,
+  onDelete,
+}: {
+  id: number;
+  name: string;
+  showDelete?: boolean;
+  onDelete?: (id: number) => void;
+}) => {
+    console.log(showDelete);
+    
+  return (
+    <Card className="p-4 relative shadow rounded  border hover:bg-gray-100 transition-all">
+      <CardContent>
+        <h3 className="text-base text-center font-medium">{name}</h3>
+      </CardContent>
+
+      {showDelete && onDelete && (
+        <div className="absolute cursor-pointer top-1 right-1">
+          <Trash className="text-red-700 w-5 h-5" onClick={() => onDelete(id)} />
+        </div>
+      )}
+    </Card>
+  );
+};
 
 export default GenreCard;
