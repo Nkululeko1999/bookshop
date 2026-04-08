dab7c31ee7926365d875a3c7028534b910a5ed0c
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2026-04-08 07:02:28 +0200

    feat: setup cloudinary and upload book image

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -1,8 +1,36 @@
 const cds = require("@sap/cds");
+const cloudinary = require("./utils/cloudinary");
 
 module.exports = class AdminService extends cds.ApplicationService {
   init() {
     const { Authors, Books, Genres } = cds.entities("AdminService");
+
+    // Upload book image
+    this.on("uploadBookImage", async (req) => {
+      const { file, bookId } = req.data;
+
+      if (!file || !bookId) {
+        return req.error("Missing book ID or file");
+      }
+
+      const book = await SELECT.from('Books').where({ ID: bookId });
+
+      if (book.imageID) {
+        await cloudinary.uploader.destroy(book.imageID);
+      }
+
+      const result = await cloudinary.uploader.upload(file, {
+        folder: "books"
+      });
+
+      await cds.run(
+        UPDATE(Books)
+        .set({ imageUrl: result.secure_url, imageID: result.public_id })
+        .where({ ID: bookId })
+      )
+
+      return { url: result.secure_url }
+    });
 
     this.before(["CREATE", "UPDATE"], Authors, async (req) => {
       console.log("Before CREATE/UPDATE Authors", req.data);
